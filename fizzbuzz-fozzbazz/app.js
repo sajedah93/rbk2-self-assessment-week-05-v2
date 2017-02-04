e8c15648de0abd289e9e28d776ce89d23c94ce1c
Sajedah.93 <your_email@whatever.comcclear>
2017-02-04 08:55:52 +0200

    sss

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -18,6 +18,68 @@ angular.module('app', ['ngRoute'])
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function(){})
-  .controller('fizzbuzzCtrl', function(){})
-  .controller('fozzbazzCtrl', function(){});
+  .factory('counter', function(){
+     var fizzz = function (count) {
+    if(count%3 ===0 && count%5 ===0){
+      return  'FizzBuzz'
+    }else if(count%3===0){
+      return  'Fizz'
+    }else if(count%5===0){
+      return  'Buzz'
+    }else {
+      return count;
+    }
+
+  }
+
+  return {
+    fizzz : fizzz
+  }
+})
+
+   .factory('another', function(){
+     var fozz = function (count) {
+    if(count%3 ===0 && count%5 ===0){
+      return  'FOZZBAZZ'
+    }else if(count%4===0){
+      return  'FOZZ'
+    }else if(count%6===0){
+      return  'BAZZ'
+    }else {
+      return count;
+    }
+
+  }
+
+  return {
+    fozz : fozz
+  }
+})
+
+  
+.controller('fizzbuzzCtrl',function($scope , counter){
+  $scope.display = 0;
+  $scope.count = 0;
+  
+  $scope.increment1 = function(){
+    $scope.count++;
+    $scope.display = counter.fizzz($scope.counter);
+  }
+}); 
+
+
+.controller('fozzbazzCtrl', function($scope,another){
+  $scope.display = 0;
+  $scope.count = 0;
+  
+  $scope.increment2 = function(){
+    $scope.count++;
+    $scope.display = another.fizzz($scope.count);
+  }
+}); 
+
+// });
+
+
+
+
